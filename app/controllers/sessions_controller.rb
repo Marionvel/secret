e1431ce1d2404f30a27ae3998aef352ae224cf70
Roger <marion.velard@gmail.com>
2018-08-02 22:11:58 +0200

    home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
    		log_in user
+      flash[:success] = "Tu es maintenant connecté(e) !"
     	redirect_to user
     else
+      flash.now[:danger] = "invalid email/password combination"
    		 render :error, template: 'pages/error'
 	end
   end
